692015d2aa3cdc10be497e4e7a2483b81c48dd65
Rajanrajsah <131872513+Rajanrajsah@users.noreply.github.com>
2025-12-27 23:41:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("active");
+}
