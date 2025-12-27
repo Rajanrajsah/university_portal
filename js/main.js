cf188455df8ed1bbe18331628ec334c379f819e6
Rajanrajsah <131872513+Rajanrajsah@users.noreply.github.com>
2025-12-27 23:13:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ const btn = document.createElement("button");
 btn.innerText = "Dark Mode";
 btn.onclick = () => document.body.classList.toggle("dark");
 document.body.prepend(btn);
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+  }
+});
