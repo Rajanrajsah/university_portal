2f2e855871325b4a189ce5f00ccecd4b348f2a43
Rajanrajsah <131872513+Rajanrajsah@users.noreply.github.com>
2025-12-28 22:31:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ function toggleMenu() {
     const nav = document.getElementById("navLinks");
     nav.classList.toggle("active");
 }
+
+async function askAI() {
+    const q = document.getElementById("aiInput").value;
+
+    const res = await fetch("http://localhost:8080/api/ai/ask", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            message: q,
+            role: "USER"
+        })
+    });
+
+    const data = await res.json();
+    document.getElementById("aiReply").innerText = data.reply;
+}
